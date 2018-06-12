ff289b31d628112d2f6c75a94bfc54064c112fcc
Taylor <Taylordent241@gmail.com>
2018-06-11 20:59:18 -0400

    removed map. Able to find name and email address

--- src/BusinessCardParser.java
+++ src/BusinessCardParser.java
@@ -1,106 +1,108 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
-
 public class BusinessCardParser {
-    public static ContactInfo getContactInfo(String document){
-        ContactInfo updatedContact= new ContactInfo();
-        
+    public static ContactInfo getContactInfo(String document) {
+        ContactInfo updatedContact = new ContactInfo();
+
         List<String> possibleNames = new ArrayList<String>();
         List<String> possiblePhoneNumbers = new ArrayList<String>();
-        
+
         Scanner scnr = new Scanner(document);
-        
-        while(scnr.hasNextLine()){
+
+        while (scnr.hasNextLine()) {
             String docLine = scnr.nextLine();
-            
-            if(updatedContact.getEmailAddress().equals("Unspecified") && verifyEmail(docLine)){
+
+            if (updatedContact.getEmailAddress().equals("Unspecified")
+                    && verifyEmail(docLine)) {
                 updatedContact.setEmailAddress(docLine);
             }
-            else if(docLine.matches(".*\\d+.*")){
+            else if (docLine.matches(".*\\d+.*")) {
                 possiblePhoneNumbers.add(docLine);
             }
-            else{
+            else {
                 possibleNames.add(docLine);
             }
-            
+
         }
-        String emailUsername = getEmailUsername(updatedContact.getEmailAddress());
+        scnr.close();
         
-        parseName(possibleNames, emailUsername);
-        
+        String emailUsername = getEmailUsername(updatedContact
+                .getEmailAddress());
+
+        updatedContact.setName(parseName(possibleNames, emailUsername));
+
+        System.out.println(updatedContact.getName() + "       "
+                + updatedContact.getEmailAddress());
         return null;
     }
-    
-    private static boolean verifyEmail(String possibleEmail){
-        if(possibleEmail.contains("@")){
-            
-            List<String> emailTLDs = new ArrayList<String>(
-                    Arrays.asList(".com", ".org", ".net", ".gov", ".edu", ".mil"));
-            
-            //returns true if the potential address has a valid Top Level Domain
-            return emailTLDs.contains(possibleEmail.substring(possibleEmail.length()-4, possibleEmail.length()));
+
+    private static boolean verifyEmail(String possibleEmail) {
+        if (possibleEmail.contains("@")) {
+
+            List<String> emailTLDs = new ArrayList<String>(Arrays.asList(
+                    ".com", ".org", ".net", ".gov", ".edu", ".mil"));
+
+            // returns true if the potential address has a valid Top Level
+            // Domain
+            return emailTLDs.contains(possibleEmail.substring(
+                    possibleEmail.length() - 4, possibleEmail.length()));
         }
-        
+
         return false;
     }
+
     private static String getEmailUsername(String emailAddress) {
-        int cutIndex =0;
-        
-        for(int i = 0; i < emailAddress.length(); i++){
-            if(emailAddress.charAt(i)=='@'){
+        int cutIndex = 0;
+
+        for (int i = 0; i < emailAddress.length(); i++) {
+            if (emailAddress.charAt(i) == '@') {
                 cutIndex = i;
                 break;
             }
         }
-        
+
         return emailAddress.substring(0, cutIndex);
     }
-    private static String parseName(List<String> possibleNames, String emailUsername){
+
+    private static String parseName(List<String> possibleNames,
+            String emailUsername) {
         String fullName = "Unspecified";
         int maxMatches = 0;
-        Map<String [], Integer> nameMatches = new HashMap<String [], Integer>();
-        for(String name: possibleNames){
-            String [] nameArray = name.split(" ");
-            int numMatches =getNumMatches(nameArray, emailUsername);
-            nameMatches.put(nameArray, numMatches);
-            if(numMatches>maxMatches){
+
+        for (String name : possibleNames) {
+
+            int numMatches = getNumMatches(name.split(" "), emailUsername);
+
+            if (numMatches > maxMatches) {
                 maxMatches = numMatches;
                 fullName = name;
             }
         }
-        System.out.println(nameMatches);
-        return "";
+        return fullName;
     }
-    
-    private static Integer getNumMatches(String [] possibleNames, String emailUsername){
+
+    private static Integer getNumMatches(String[] possibleNames,
+            String emailUsername) {
         int numMatches = 0;
-        for(String nameSegment: possibleNames){
-            if(emailUsername.toLowerCase().contains(nameSegment.toLowerCase())){
+        for (String nameSegment : possibleNames) {
+            if (emailUsername.toLowerCase()
+                    .contains(nameSegment.toLowerCase())) {
                 numMatches++;
             }
         }
         return numMatches;
-        
+
     }
-    
-    
-    public static void main(String [] args){
-        //For testing purposes only
-        String docToParse = "Bob Smith"
-                + "\nSoftware Engineer"
-                + "\nDecision &amp; Security Technologies"
-                + "\nABC Technologies"
-                + "\n123 North 11th Street"
-                + "\nSuite 229"
-                + "\nArlington, VA 22209"
-                + "\nTel: +1 (703) 555-1259"
-                + "\nFax: +1 (703) 555-1200"
-                + "\nbobSmith@acmetech.com";
+
+    public static void main(String[] args) {
+        // For testing purposes only
+        String docToParse = "Acme Technologies" + "\nAnalytic Developer"
+                + "\nJane Doe" + "\n1234 Roadrunner Way"
+                + "\nColumbia, MD 12345" + "\nPhone: 410-555-1234"
+                + "\nFax: 410-555-4321" + "\nJane.doe@acmetech.com";
         ContactInfo info = getContactInfo(docToParse);
     }
 }
